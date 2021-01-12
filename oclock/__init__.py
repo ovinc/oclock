44c4dcab5ea0723385c1e17e01a3dc98db9bc86a
Olivier Vincent <olivier.vincent@univ-lyon1.fr>
2021-01-12 19:12:45 +0100

    Define __version__ automatically in __init__.py

--- oclock/__init__.py
+++ oclock/__init__.py
@@ -4,3 +4,7 @@ from .timer import Timer
 from .countdown import Countdown
 from .general import parse_time, measure_time, measure_duration
 from .loop import loop, interactiveloop
+
+from importlib.metadata import version
+
+__version__ = version('oclock')
